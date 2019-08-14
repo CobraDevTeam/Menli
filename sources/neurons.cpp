ad81ee82c14a9fa103efe591c760ad2b24ffbf5b
PtichapronRouge <benjamin.mouscadet@supelec.fr>
2019-08-14 23:10:04 +1200

    ENH: added homogeneity-enforcing

--- sources/neurons.cpp
+++ sources/neurons.cpp
@@ -15,19 +15,27 @@
 namespace cg
 {
 
-Group::Group(unsigned int nb_neurons, double init_pot)
-: m_potentials(nb_neurons, init_pot)
-, m_diff([] (double dt) {return 1.0 * dt;})
+Group::Group(unsigned int nb_neurons, double init_pot, std::function<double(double, double)> df)
+: m_potentials(nb_neurons, Potential(init_pot))
+, m_diff([df] (Time t, Potential p) {return DPotDt(df(t.val, p.val));})
 , m_threshold(1.0) // TODO hardcoded value
 {}
+
+Group::Group(unsigned int nb_neurons, double init_pot)
+: Group(nb_neurons, init_pot, [] (double, double p) {return p * 1.0;})
+{}
+
+Group::Potential operator*(Group::Time lhs, Group::DPotDt rhs) {return Group::Potential(lhs.val * rhs.val);}
 
 std::vector<bool> Group::step(double dt)
 {
     std::vector<bool> fires(m_potentials.size(), false);
     for(size_t i = 0; i < m_potentials.size(); ++i) {
-        m_potentials[i] += m_diff(dt);
-        if (m_potentials[i] > m_threshold) {
-            m_potentials[i] = 0.0;
+        // MOST BASIC implementation of Euler's algorithm
+        // Given df/dt = g(t, f(t)), assumes that g(t,f(t)) = h(f(t)) (independent of time)
+        m_potentials[i] += Time(dt) * m_diff(Time(0.0), m_potentials[i]);
+        if (m_potentials[i].val > m_threshold) {
+            m_potentials[i] = Potential(0.0);
             fires[i] = true;
         }
     }
@@ -40,7 +48,7 @@ void Group::integrate_spike(std::vector<bool> targets, double weight)
     assert(targets.size() == m_potentials.size());
     for (size_t i =0; i < m_potentials.size(); ++i) {
         if(targets[i]) {
-            m_potentials[i] += weight;
+            m_potentials[i] += Potential(weight);
         }
     }
 }
--- sources/neurons.hpp
+++ sources/neurons.hpp
@@ -21,21 +21,40 @@ class Graph;
 class Group
 {
     public:
+        // Numerical stuff
+        // Should really be put in its own file and namespace
+        // Is a really lazy and basic implementation of homogeneity enforcing
+        template <typename Phantom>
+        struct Named {
+            explicit Named(double v): val(v) {}
+            Named& operator=(const Named& x) {val = x.val; return *this;}
+            void   operator+=(const Named& x) {val += x.val;}
+            double val;
+        };
+
+        using Potential = Named<struct PotParam>;
+        using Time      = Named<struct TParam>;
+        using DPotDt    = Named<struct DPotDtParam>;
+
+
         using Ptr = std::shared_ptr<Group>;
+        using DfType = std::function<DPotDt(Time, Potential)>;
 
     public:
         Group(unsigned int nb_neurons, double init_pot);
+        Group(  unsigned int nb_neurons,
+                double init_pot,
+                std::function<double(double, double)>);
 
         std::vector<bool>   step(double dt);
         void                integrate_spike(std::vector<bool> targets, double weight);
         unsigned int        count() const;
 
     private:
-        std::vector<double>     m_potentials;
-        std::function<double(double)> m_diff;
-        double                          m_threshold;
+        std::vector<Potential>  m_potentials;
+        const DfType            m_diff;
+        const double            m_threshold;
 };
-
 template <typename ...Args>
 Group::Ptr make_group(Args... args)
 {
